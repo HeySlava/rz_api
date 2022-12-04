3d8c5562817651e4f004b406291477c6e6f7eedf
Vyacheslav Kapitonov <vyach.kapitonov@gmail.com>
2022-12-04 14:43:25 +0300

    sqlalchemy into pydantic

--- views.py
+++ views.py
@@ -1,44 +1,23 @@
-import schemes
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
+from models import schemes
+import services
+from data.db_session import create_session
+
+from models.pydantic_models import PydanticDatamart, PydanticMethodWithParams
 
 
 router = APIRouter()
 
 
-
 @router.get('/getMethods', response_model=list[schemes.MethodModel])
-async def get_methods():
-    return [
-            {
-                'id': 1,
-                'method': 'method1',
-                'params': [
-                    {'id': 1, 'param': 'param1'},
-                    {'id': 2, 'param': 'param2'}
-                ]
-            },
-            {
-                'id': 2,
-                'method': 'method2',
-                'params': [
-                    {'id': 1, 'param': 'param1'},
-                    {'id': 2, 'param': 'param2'}
-                ]
-            }
-        ]
-
+async def get_methods(db = Depends(create_session)):
+    methods = services.get_methods(db)
+    return [PydanticMethodWithParams.from_orm(m).dict() for m in methods]
 
 
 @router.get('/getDatamarts', response_model=list[schemes.DatamartModel])
-async def get_datamarts():
-    return [
-            {
-                'id': 1,
-                'datamart': 'datamart1'
-            },
-            {
-                'id': 2,
-                'datamart': 'datamart1'
-            }
-        ]
+async def get_datamarts(db = Depends(create_session)):
+
+    datamarts = services.get_datamarts(db)
+    return  [PydanticDatamart.from_orm(d).dict() for d in datamarts]
