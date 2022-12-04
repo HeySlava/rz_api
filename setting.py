e59b655508ca5667bb9e7a486a5f7191edf8a54f
Vyacheslav Kapitonov <vyach.kapitonov@gmail.com>
2022-12-04 14:42:19 +0300

    rename

--- setting.py
+++ setting.py
@@ -4,12 +4,15 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     port: int = 8080
     host: str = '0.0.0.0'
+    debug: bool = False
+    conn_str: str = 'sqlite:///:memory:'
 
     class Config:
         env_prefix = 'rz_'
 
 
 settings = Settings(
+        conn_str='sqlite:///rz.sqlite',
         _env_file='.env',
         _env_file_encoding='utf-8'
     )
