19496325e2e2d30cde5af4cbfbc2016e52190384
Vyacheslav Kapitonov <vyach.kapitonov@gmail.com>
2022-12-14 12:17:29 +0300

    added dummy endpoint

--- main.py
+++ main.py
@@ -2,11 +2,23 @@ import views
 import uvicorn
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from setting import settings
 from data import db_session
 
-
 app = FastAPI()
+
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 def routes_configure():
