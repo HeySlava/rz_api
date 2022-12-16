719a976d9b08e9be34217ebfae0b114d327f219c
Vyacheslav Kapitonov <vyach.kapitonov@gmail.com>
2022-12-16 20:00:44 +0300

    added foreign key

--- models/pydantic_models.py
+++ models/pydantic_models.py
@@ -2,13 +2,14 @@ from data.models import Method, Datamart, Param
 from pydantic_sqlalchemy import sqlalchemy_to_pydantic
 
 
+PydanticDatamart = sqlalchemy_to_pydantic(Datamart)
 PydanticParam = sqlalchemy_to_pydantic(Param)
-
 PydanticMethod = sqlalchemy_to_pydantic(Method)
+
 
 class PydanticMethodWithParams(PydanticMethod):
     params: list[PydanticParam]
 
 
-
-PydanticDatamart = sqlalchemy_to_pydantic(Datamart)
+class PydanticDatamartsWithMethods(PydanticDatamart):
+    methods: list[PydanticMethodWithParams]
